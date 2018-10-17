0ce426bafffec417c24d09cb6799e3e2bb4891f6
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-10-17 13:27:46 -0700

    added working directory + file paths

--- scripts/2018-10-12-Bismark-Revised-Parameters.sh
+++ scripts/2018-10-12-Bismark-Revised-Parameters.sh
@@ -15,13 +15,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yaaminiv@uw.edu
 ## Specify the working directory for this job
-#SBATCH --workdir=/gscratch/scrubbed/yaaminiv/2018-10-12-Virginica-Bismark-Revised-Parameters
+#SBATCH --workdir=/gscratch/scrubbed/yaamini/Virginica-MBD/2018-10-17-Virginica-Bismark-Revised-Parameters
 
 #Add Directories and Programs to Path
 
 bismark_dir="/gscratch/srlab/programs/Bismark-0.19.0"
-trimmed_files=
-genome=
+trimmed_files="/gscratch/scrubbed/yaamini/data/Virginica-MBD/2018-10-17-Trimmed-Files/"
+genome="/gscratch/scrubbed/yaamini/data/Virginica-MBD/2018-04-27-Bismark-Inputs/"
 samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
 
 #Genome preparation
@@ -37,7 +37,7 @@ find ${trimmed_files}/zr2096_*R1*.fq.gz \
 --genome ${genome} \
 -1 ${trimmed_files}/{}_s1_R1_val_1.fq.gz \
 -2 ${trimmed_files}/{}_s1_R2_val_2.fq.gz \
-2> bismark-1012.err
+2> bismark-1017.err
 
 #Deduplication
 
@@ -45,7 +45,7 @@ ${bismark_dir}/deduplicate_bismark \
 -p \
 --bam \
 zr2096_*_s1_R1_val_1_bismark_bt2_pe.bam \
-2> dedup-1012.err
+2> dedup-1017.err
 
 #Sorting for Downstream Applications
 
@@ -69,7 +69,7 @@ ${bismark_dir}/bismark_methylation_extractor \
 --scaffolds \
 --multicore 28 \
 *deduplicated.bam \
-2> bme-1012.err
+2> bme-1017.err
 
 #HTML Processing Report
 
