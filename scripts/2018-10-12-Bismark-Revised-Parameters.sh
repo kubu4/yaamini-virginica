6c488c5d9701ef55a5a1943995a2acd01ba2c29d
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-10-25 09:57:09 -0700

    added samtools and bowtie paths, changed SBATCH directory

--- scripts/2018-10-12-Bismark-Revised-Parameters.sh
+++ scripts/2018-10-12-Bismark-Revised-Parameters.sh
@@ -15,14 +15,15 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yaaminiv@uw.edu
 ## Specify the working directory for this job
-#SBATCH --workdir=/gscratch/scrubbed/yaamini/data/Virginica-MBD/2018-10-17-Virginica-Bismark-Revised-Parameters
+#SBATCH --workdir=/gscratch/scrubbed/yaamini/analyses/Virginica-MBD/2018-10-17-Virginica-Bismark-Revised-Parameters
 
 #Add Directories and Programs to Path
 
-bismark_dir="/gscratch/srlab/programs/Bismark-0.19.0"
+bismark_dir="/gscratch/srlab/programs/Bismark-0.19.0/"
+bowtie2_dir="/gscratch/srlab/programs/bowtie2-2.3.4.1-linux-x86_64/"
 trimmed_files="/gscratch/scrubbed/yaamini/data/Virginica-MBD/2018-10-17-Trimmed-Files/"
 genome="/gscratch/scrubbed/yaamini/data/Virginica-MBD/2018-04-27-Bismark-Inputs/"
-samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
+samtools="/gscratch/srlab/programs/samtools-1.9/"
 
 #Genome preparation
 #YRV prepared the bisulfite genome on hummingbird: https://github.com/RobertsLab/project-virginica-oa/blob/master/notebooks/2018-04-27-Gonad-Methylation-Bismark.ipynb
@@ -31,13 +32,13 @@ samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
 
 find ${trimmed_files}/zr2096_*R1*.fq.gz \
 | xargs basename -s _s1_R1_val_1.fq.gz | xargs -I{} ${bismark_dir}/bismark \
+--path_to_bowtie ${bowtie2_dir} \
 --non_directional \
 -p 28 \
 -score_min L,0,-1.2 \
 --genome ${genome} \
 -1 ${trimmed_files}/{}_s1_R1_val_1.fq.gz \
 -2 ${trimmed_files}/{}_s1_R2_val_2.fq.gz \
-2> bismark-1017.err
 
 #Deduplication
 
@@ -45,12 +46,11 @@ ${bismark_dir}/deduplicate_bismark \
 -p \
 --bam \
 zr2096_*_s1_R1_val_1_bismark_bt2_pe.bam \
-2> dedup-1017.err
 
 #Sorting for Downstream Applications
 
 find *deduplicated.bam \
-| xargs basename -s _s1_R1_val_1_bismark_bt2_pe.deduplicated.bam | xargs -I{} /Users/Shared/Apps/samtools-1.8/samtools \
+| xargs basename -s _s1_R1_val_1_bismark_bt2_pe.deduplicated.bam | xargs -I{} ${samtools}/samtools \
 sort {}_s1_R1_val_1_bismark_bt2_pe.deduplicated.bam \
 -o {}_dedup.sorted.bam
 
@@ -69,7 +69,6 @@ ${bismark_dir}/bismark_methylation_extractor \
 --scaffolds \
 --multicore 28 \
 *deduplicated.bam \
-2> bme-1017.err
 
 #HTML Processing Report
 
